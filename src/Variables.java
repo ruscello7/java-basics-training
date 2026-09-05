d71066e02d77ca9ea067c6ccdf6941f2f5206b58
Matheus Ribeiro <ribeiro7aith@gmail.com>
2026-09-05 01:39:56 -0300

    feat: add variables and primitive types exercise

---
+++ src/Variables.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        String name;
+        int age;
+        double height;
+        String student;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+
+        System.out.print("Enter your Height: ");
+        height = input.nextDouble();
+
+        input.nextLine();
+        System.out.print("You are a student: ");
+        student = input.nextLine();
+
+        System.out.printf("Your name is: %s%n Your Age is: %d%n Your Height is: %.2f%n You a student: %s%n ", name, age, height, student);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
