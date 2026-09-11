add4d310bd145deb1deba7c74e1713bc49f27c60
Matheus Ribeiro <ribeiro7aith@gmail.com>
2026-09-11 12:38:10 -0300

    feat: add an exercise using a method that compares numbers and returns the larger one

---
+++ src/Methods.java
@@ -0,0 +1,18 @@
+public class Methods {
+
+    static int maior (int a, int b) {
+        if (a > b) {
+            return a; }
+        else {
+                return b;
+            }
+        }
+
+    public static void main(String[] args) {
+        System.out.println(maior(10, 20));
+        System.out.println(maior(15, 25));
+        System.out.println(maior(5, 10));
+
+    }
+
+    }
